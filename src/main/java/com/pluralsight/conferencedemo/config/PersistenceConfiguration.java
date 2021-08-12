1d38d150c29ce73e3ae6e270a6ba789857d5fbb8
acarretero <acarretero@sicaman.com>
2021-08-12 09:52:38 +0200

    Initial commit

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,27 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+//    @Value("${spring.datasource.username}")
+//    private String username;
+//
+//    @Value("${spring.datasource.password}")
+//    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("Welcome");
+        System.out.println("datasource called");
+        return builder.build();
+    }
+}
---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,48 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable long id) {
+        return sessionRepository.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable long id) {
+        // todo: check if there are children and delete them
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable long id, @RequestBody Session session) {
+        // todo: return 400 if parameter missing
+        Session existingSession = sessionRepository.getById(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
+    }
+}
